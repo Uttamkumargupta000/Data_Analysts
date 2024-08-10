a4af25ad9027266d4cba266f598c475633e6f4ae
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2024-08-11 00:20:15 +0530

    Aggregate_Function_Leetcode

---
+++ Leetcode_SQL50/18_1633.sql
@@ -0,0 +1,10 @@
+-- # Write your MySQL query statement below
+
+-- using aggregate function and round amd distinct 
+
+
+select contest_id, 
+round(count(distinct user_id) * 100 / (select count(user_id) from Users),2) as percentage
+from Register
+group by contest_id
+order by percentage desc, contest_id
